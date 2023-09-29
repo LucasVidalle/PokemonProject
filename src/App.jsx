6835ccacd716549cbf2f4c3430e7ba2387f726d8
LucasVidalle <lucas.vidalle@gm2dev.com>
2023-09-29 12:02:08 -0300

    feat: progress with stats view

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import { Card } from "./assets/Components/Card.jsx";
 import { Nav } from "./assets/Components/Nav.jsx";
+import { Info } from "./assets/Components/Info.jsx";
 
 function App() {
 
   return (
     <div className='all-content'>
-      <Nav />
-      <Card />
+      {/* <Nav />
+      <Card /> */}
+      <Info />
     </div>
   )
 }
